8053daa969a9d826779b0d6dfb75474a318e239b
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2025-05-12 00:23:49 +0800

    remove stripe version

--- packages/stripe/index.ts
+++ packages/stripe/index.ts
@@ -7,8 +7,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
   );
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-03-31.basil",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default stripe;
