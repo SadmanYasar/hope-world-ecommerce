ff2e56ccaae5c1b693297c7f09150ee935d8303c
Abby <wnabihah09@gmail.com>
2025-07-09 05:18:40 +0800

    created maven project

---
+++ spring/hopeworld/src/main/java/com/hopeworld/hopeworld/HopeworldApplication.java
@@ -0,0 +1,13 @@
+package com.hopeworld.hopeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeworldApplication.class, args);
+	}
+
+}
